597d623211c5a12487842317ddfce8f0cd3dd272
Sushanth PS <35593841+SushanthPS@users.noreply.github.com>
2021-11-28 20:24:22 +0530

    Add files via upload

--- nQueen.js
+++ nQueen.js
@@ -1,50 +1,41 @@
 let ans = 0;
 
 function isQueenSafe(chess, row, col) {
-    //vertical
-    for (let i = row - 1, j = col; i >= 0; i--) {
-        if (chess[i][j] == 1)
-            return false;
-    }
+    //not required to check any squares to the right or below as it's not filled yet
 
-    //left top diagonal
-    for (let i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
-        if (chess[i][j] == 1)
-            return false;
-    }
+    //check vertical top
+    for (let i = row - 1, j = col; i >= 0; i--)
+        if (chess[i][j] === 1) return false;
 
-    //right top diagonal
-    for (let i = row - 1, j = col + 1; i >= 0 && j < 10; i--, j++) {
-        if (chess[i][j] == 1)
-            return false;
-    }
+    //check top-left diagonal
+    for (let i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--)
+        if (chess[i][j] === 1) return false;
+
+    //check top-right diagonal
+    for (let i = row - 1, j = col + 1; i >= 0 && j < chess[0].length; i--, j++)
+        if (chess[i][j] === 1) return false;
 
     return true;
 }
 
 function nQueen(chess, row) {
-    if (row >= chess.length) {
-        ans++;
-    }
-    for (let col = 0; col < chess.length; col++) {
+    //if row is equal to chess.length, then we filled in all the queens
+    if (row >= chess.length) ans++;
+
+    for (let col = 0; col < chess[0].length; col++) {
         if (isQueenSafe(chess, row, col)) {
             chess[row][col] = 1;
-            nQueen(chess, row + 1)
-            chess[row][col] = 0
+            nQueen(chess, row + 1);
+            chess[row][col] = 0;
         }
     }
 }
 
 function runProgram(input) {
     let n = Number(input);
-    let chess = [];
-    for (let i = 0; i < n; i++) {
-        let temp = new Array(n).fill(0);
-        chess.push(temp);
-    }
+    let chess = new Array(n).fill(0).map((el) => new Array(n).fill(0));
     nQueen(chess, 0);
     console.log(ans);
-
 }
 if (process.env.USERNAME === "getsu") {
     runProgram(`4`);
