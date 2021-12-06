298a45f22f35e05ed26532c044c949dfeae9387c
Sushanth PS <35593841+SushanthPS@users.noreply.github.com>
2021-12-06 21:04:22 +0530

    Add files via upload

--- nQueen.js
+++ nQueen.js
@@ -1,28 +1,28 @@
 let ans = 0;
 
 function isQueenSafe(chess, row, col) {
-    //not required to check any squares to the right or below as it's not filled yet
+    //not required to check down or right as nothing is filled yet.
 
-    //check vertical top
+    //vertical right
     for (let i = row - 1, j = col; i >= 0; i--)
-        if (chess[i][j] === 1) return false;
+        if (chess[i][j] == 1) return false;
 
-    //check top-left diagonal
+    //left top diagonal
     for (let i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--)
-        if (chess[i][j] === 1) return false;
+        if (chess[i][j] == 1) return false;
 
-    //check top-right diagonal
-    for (let i = row - 1, j = col + 1; i >= 0 && j < chess[0].length; i--, j++)
-        if (chess[i][j] === 1) return false;
+    //right top diagonal
+    for (let i = row - 1, j = col + 1; i >= 0 && j < 10; i--, j++)
+        if (chess[i][j] == 1) return false;
 
     return true;
 }
 
 function nQueen(chess, row) {
-    //if row is equal to chess.length, then we filled in all the queens
-    if (row >= chess.length) ans++;
-
-    for (let col = 0; col < chess[0].length; col++) {
+    if (row >= chess.length) {
+        ans++;
+    }
+    for (let col = 0; col < chess.length; col++) {
         if (isQueenSafe(chess, row, col)) {
             chess[row][col] = 1;
             nQueen(chess, row + 1);
@@ -33,7 +33,11 @@ function nQueen(chess, row) {
 
 function runProgram(input) {
     let n = Number(input);
-    let chess = new Array(n).fill(0).map((el) => new Array(n).fill(0));
+    let chess = [];
+    for (let i = 0; i < n; i++) {
+        let temp = new Array(n).fill(0);
+        chess.push(temp);
+    }
     nQueen(chess, 0);
     console.log(ans);
 }
